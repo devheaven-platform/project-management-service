5a03ff415577fa33ebe5accb3c30de15d90c20a1
drokii <pedrodossantosmarques@gmail.com>
2019-03-20 15:42:11 +0100

    added create project controller binding

--- src/main/java/controllers/ProjectController.java
+++ src/main/java/controllers/ProjectController.java
@@ -1,4 +1,30 @@
 package controllers;
 
+import models.Project;
+import services.ProjectService;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Produces({"application/json"})
+@Path("project")
 public class ProjectController {
+
+    @Inject
+    ProjectService projectService;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/createproject")
+    public Response createProject(Project project) {
+        projectService.createProject(project);
+        return Response.status(201).entity(project).build();
+    }
+
+
 }
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Pedro
+  Date: 3/20/2019
+  Time: 3:02 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
