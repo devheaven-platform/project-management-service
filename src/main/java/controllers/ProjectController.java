d92b5c4c29be93ed5e7319c975355d06e108c791
Frank <f.broeks@student.fontys.nl>
2019-03-26 12:53:13 +0100

    Add Edit project method in ProjectController

--- src/main/java/controllers/ProjectController.java
+++ src/main/java/controllers/ProjectController.java
@@ -1,15 +1,16 @@
 package controllers;
 
+import dto.ProjectDTO;
 import models.Project;
 import services.ProjectService;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Produces({"application/json"})
 @Path("project")
@@ -26,5 +27,33 @@ public class ProjectController {
         return Response.status(201).entity(project).build();
     }
 
+    @PATCH
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/")
+    public Response editProject(ProjectDTO projectDTO){
+        try{
+            Project project = projectService.getProjectById(projectDTO.getId());
 
+            List<UUID> UUIDMembers = new ArrayList<>();
+            for(String member : projectDTO.getMembers()){
+                UUIDMembers.add(UUID.fromString(member));
+            }
+            project.setMembers(UUIDMembers);
+            project.setBudget(projectDTO.getBudget());
+            project.setName(projectDTO.getName());
+            project.setArchived(projectDTO.isArchived());
+            project.setClient(projectDTO.getClient());
+            project.setDeadlines(projectDTO.getDeadlines());
+            project.setEndDate(projectDTO.getEndDate());
+            project.setStartDate(projectDTO.getStartDate());
+            project.setDescription(projectDTO.getDescription());
+            project.setOwner(projectDTO.getOwner());
+
+            projectService.editProject(project);
+            return Response.ok().build();
+        }catch(Exception ex){
+
+            return Response.status(500).build();
+        }
+    }
 }
