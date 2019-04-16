345a8f489054764079d8e5f605f6a8cd8535f146
tomdewildt <t.dewildt@student.fontys.nl>
2019-04-16 10:31:26 +0200

    WIP: working on project service

--- src/main/java/com/devheaven/service/repositories/ProjectRepository.java
+++ src/main/java/com/devheaven/service/repositories/ProjectRepository.java
@@ -1,4 +1,15 @@
 package com.devheaven.service.repositories;
 
-public class ProjectRepository {
+import com.devheaven.service.models.Project;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, UUID> {
+
+    List<Project> findByMembersContains(String member, Sort sort);
 }
--- src/main/java/com/devheaven/service/services/ProjectService.java
+++ src/main/java/com/devheaven/service/services/ProjectService.java
@@ -1,4 +1,32 @@
 package com.devheaven.service.services;
 
+import com.devheaven.service.models.Project;
+import com.devheaven.service.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> findAll() {
+        return projectRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+    }
+
+    public List<Project> findForUser(String id) {
+        return projectRepository.findByMembersContains(id, Sort.by(Sort.Direction.ASC, "name"));
+    }
+
+
+
+
 }
