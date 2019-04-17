edea91daca149adfe87c654c295a2d4215ed778b
tomdewildt <t.dewildt@student.fontys.nl>
2019-04-17 20:45:08 +0200

    Add milestone unit tests

--- src/test/java/com/devheaven/service/services/MilestoneServiceTest.java
+++ src/test/java/com/devheaven/service/services/MilestoneServiceTest.java
@@ -15,10 +15,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.notNull;
 
 
 @RunWith(SpringRunner.class)
@@ -48,53 +48,89 @@ public class MilestoneServiceTest {
     @MockBean
     private MilestoneRepository milestoneRepository;
 
+    private Project project;
+    private Milestone milestone;
+
     @Before
     public void setUp() {
-        Project project = new Project();
-        project.setName("TestProject");
+        project = new Project();
+        project.setId(UUID.fromString("cf023055-af8e-42bf-9d2e-cfe37cefa237"));
+        project.setName("Project 1");
+        project.setDescription("Description 1");
+        project.setBudget(10);
+        project.setStart(new Date());
         project.setArchived(false);
-        project.setBudget(1000);
-        project.setClient(UUID.fromString("cf023055-af8e-42bf-9d2e-cfe37cefa237"));
-        project.setCreatedAt(new Date());
-        project.setId(UUID.fromString("cf023055-af8e-42bf-9d2e-cfe37cefaeee"));
-        project.setOwner(UUID.fromString("cf023055-af8e-42bf-9d2e-cfe37cefaddd"));
+        project.addMember(UUID.fromString("4562366c-2d59-44a5-b385-8848144706fe"));
 
-        Mockito.when(projectRepository.findById(UUID.fromString("cf023055-af8e-42bf-9d2e-cfe37cefaeee"))).thenReturn(Optional.of(project));
+        Mockito.when(projectRepository.findById(UUID.fromString("cf023055-af8e-42bf-9d2e-cfe37cefa237"))).thenReturn(Optional.of(project));
 
-        Milestone milestone = new Milestone();
-        milestone.setName("TestMilestone");
-        milestone.setDescription("TestDescription");
-        milestone.setDate(new Date());
+        milestone = new Milestone();
         milestone.setId(UUID.fromString("cf023055-af8e-42bf-9d2e-cfe37cefaFFF"));
+        milestone.setName("Milestone 1");
+        milestone.setDescription("Description 1");
+        milestone.setDate(new Date());
 
-        Mockito.when(milestoneRepository.findById(UUID.fromString("cf023055-af8e-42bf-9d2e-cfe37cefaFFF"))).thenReturn(Optional.of(milestone));
         Mockito.when(milestoneRepository.findAll(Sort.by(Sort.Direction.DESC, "date"))).thenReturn(new ArrayList<Milestone>(){{
             add(milestone);
         }});
+
+        Mockito.when(milestoneRepository.findById(UUID.fromString("cf023055-af8e-42bf-9d2e-cfe37cefaFFF"))).thenReturn(Optional.of(milestone));
+        Mockito.when(milestoneRepository.findById(UUID.fromString("b9c0dce0-b047-43f8-960d-53aca1a9fa26"))).thenReturn(Optional.empty());
+
+        Mockito.when(milestoneRepository.save(notNull())).thenReturn(milestone);
     }
 
     @Test
     public void findAll() {
-        throw new UnsupportedOperationException();
+        List<Milestone> milestones = milestoneService.findAll();
+
+        assertEquals("Service returned an invalid milestone count", 1, milestones.size());
+        assertEquals("Milestone has an invalid name", "Milestone 1", milestones.get(0).getName());
+        assertEquals("Milestone has an invalid description", "Description 1", milestones.get(0).getDescription());
     }
 
     @Test
     public void getMilestoneById() {
-        throw new UnsupportedOperationException();
+        Milestone milestone = milestoneService.findById(UUID.fromString("cf023055-af8e-42bf-9d2e-cfe37cefaFFF"));
+
+        assertEquals("Milestone has an invalid name", "Milestone 1", milestone.getName());
+        assertEquals("Milestone has an invalid description", "Description 1", milestone.getDescription());
+    }
+
+    @Test
+    public void getMilestoneByIdWrongId() {
+        Milestone milestone = milestoneService.findById(UUID.fromString("b9c0dce0-b047-43f8-960d-53aca1a9fa26"));
+
+        assertNull("Milestone should be null", milestone);
     }
 
     @Test
     public void createMilestone() {
-        throw new UnsupportedOperationException();
+        Project project = projectService.findById(UUID.fromString("cf023055-af8e-42bf-9d2e-cfe37cefa237"));
+        Milestone newMilestone = new Milestone();
+        newMilestone.setName("Milestone 1");
+        newMilestone.setDescription("Description 1");
+        newMilestone.setDate(new Date());
+        Milestone milestone = milestoneService.createMilestone(project, newMilestone);
+
+
+        assertEquals("Milestone has an invalid name", "Milestone 1", milestone.getName());
+        assertEquals("Milestone has an invalid description", "Description 1", milestone.getDescription());
     }
 
     @Test
     public void updateMilestone() {
-        throw new UnsupportedOperationException();
+        Milestone updateMilestone = new Milestone();
+        updateMilestone.setName("Milestone 1");
+        Milestone milestone = milestoneService.updateMilestone(updateMilestone);
+
+
+        assertEquals("Milestone has an invalid name", "Milestone 1", milestone.getName());
+        assertEquals("Milestone has an invalid description", "Description 1", milestone.getDescription());
     }
 
     @Test
     public void deleteMilestone() {
-        throw new UnsupportedOperationException();
+        milestoneService.deleteMilestone(milestone);
     }
 }
