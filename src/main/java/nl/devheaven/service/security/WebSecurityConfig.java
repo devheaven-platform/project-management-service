f4bde1c3e15b9df4ea3d95c8d4c5e47ce9032a99
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2019-04-23 11:05:03 +0200

    Fix cors origins error (#23)

--- src/main/java/nl/devheaven/service/security/WebSecurityConfig.java
+++ src/main/java/nl/devheaven/service/security/WebSecurityConfig.java
@@ -66,10 +66,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Collections.singletonList("x-auth-token"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
