8885b16d4e2595ff6f6c33cc19eb1acb56543510
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2019-05-28 10:04:21 +0200

    Add request logging to spring (#34)
    * Add request logging to spring
    
    * Update application-dev.yml

---
+++ src/main/java/nl/devheaven/service/configuration/RequestLoggingConfig.java
@@ -0,0 +1,19 @@
+package nl.devheaven.service.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeHeaders(true);
+        return filter;
+    }
+}
